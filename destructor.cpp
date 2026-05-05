7f5eb4120e4140fe785f3b844c374c3c4d8c86a8
suhilmi01 <suhilmi01@mail.com>
2026-05-05 16:35:49 +0700

    membuat class angka

---
+++ destructor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class angka
+{
+private:
+    int *arr;
+    int panjang;
+
+public:
+    angka(int); // Constructor
+    ~angka();   // Destructor
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i) { // Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka() { // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
