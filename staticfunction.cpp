d95ec4d72e90ab5cf32cf602cbe6358739d80173
suhilmi01 <suhilmi01@mail.com>
2026-05-05 16:11:01 +0700

    membuat prosedur diluar kelas

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -28,3 +28,15 @@ public:
     };
 };
 
+int Mahasiswa::nim = 0;
+
+//pendefinisian prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
