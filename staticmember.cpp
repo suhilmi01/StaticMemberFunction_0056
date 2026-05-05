ae1affa99003e9936a9f0280feb402ae0f5b7789
suhilmi01 <suhilmi01@mail.com>
2026-05-05 15:59:34 +0700

    variabel nim di class mahasiswa

--- staticmember.cpp
+++ staticmember.cpp
@@ -19,3 +19,16 @@ public:
         setID();
     }
 };
+
+int Mahasiswa::nim = 10;
+
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+};
+
+void Mahasiswa::printAll() {
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
