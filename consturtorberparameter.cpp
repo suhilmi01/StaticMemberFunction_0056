429057019c3736209119a66d4afa0091800bcfe8
suhilmi01 <suhilmi01@mail.com>
2026-05-05 16:25:27 +0700

    fungsi utama dan prgram utama

--- consturtorberparameter.cpp
+++ consturtorberparameter.cpp
@@ -15,3 +15,9 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "Nama : " << nama << endl;
 }
 
+int main(){
+    
+     mahasiswa mhs(12345, "rizki tukang makan"); // memanggil constructor dengan parameter
+    return 0;
+    
+}
