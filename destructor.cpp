efbcbdbb79fd9406234fc12f57b5d865f1d9121b
suhilmi01 <suhilmi01@mail.com>
2026-05-05 16:36:16 +0700

    membuat void

--- destructor.cpp
+++ destructor.cpp
@@ -27,3 +27,20 @@ angka::~angka() { // Destructor
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
